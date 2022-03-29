2a6413d93f30f75cb3ba125a5fc1ab10f8c6156e
otienonick <nick.odongi@student.moringaschool.com>
2022-03-29 13:51:10 +0300

    style navbar and make it responsive

---
+++ script.js
@@ -0,0 +1,18 @@
+// navbar scroll
+$(document).ready(function(){
+$(window).scroll(function(){
+    if(this.scrollY > 30){
+        $('.navbar').addClass('sticky'); 
+    }else{
+        $('.navbar').removeClass('sticky'); 
+    }
+});
+
+// toggle menubar
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+
+})
+
+});
