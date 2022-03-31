0653dd9f0ca4d3fcebc1e78308a9dc56c70aa281
otienonick <nick.odongi@student.moringaschool.com>
2022-03-31 08:52:46 +0300

    add typing animation

--- script.js
+++ script.js
@@ -6,12 +6,42 @@ $(window).scroll(function(){
     }else{
         $('.navbar').removeClass('sticky'); 
     }
+    if(this.scrollY > 500){
+        $('.scroll-up-btn').addClass('show');
+
+    }else{
+        $('.scroll-up-btn').removeClass('show');
+            
+    } 
 });
+
+// slide-up script
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop:0});
+});
+
+
 
 // toggle menubar
 $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass('active');
     $('.menu-btn i').toggleClass('active');
+
+});
+
+// typing animation script
+ var typed = new Typed('.typing',{
+     strings:['Frontend Developer.','Backend Developer.','UI/UX Designer.'],
+     typeSpeed: 100,
+     backSpeed: 50,
+     loop: true
+
+ });
+ var typed = new Typed('.typing-2',{
+    strings:['Frontend Developer.','Backend Developer.','UI/UX Designer.'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
 
 });
 
